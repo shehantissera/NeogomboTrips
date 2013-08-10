dad03e38194c2ad0cc14c4b6421e6c138cec1383
Shehan Tis <shehanproductions@ymail.com>
2013-08-10 14:31:27 +0530

    Initiation

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Testing website</title>
+    </head>
+    <body>
+        <h1>Testing website</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
